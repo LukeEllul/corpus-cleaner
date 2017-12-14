1ddee7a3b22c6dbe5be52709212e35a634f0a37c
LukeEllul <ellul.luke@gmail.com>
2017-12-14 01:36:31 +0100

    cleaned some files and directories

--- cleaner.js
+++ cleaner.js
@@ -2,24 +2,6 @@ const cheerio = require('cheerio');
 const {iterateThroughFiles} = require('../fileOps');
 const fs = require('fs');
 
-// const exampleTextPath = './texts/A/A1/A1B.xml';
-
-// const text = fs.readFileSync(exampleTextPath).toString();
-
-// const $ = cheerio.load(text, {
-//     xmlMode: true
-// });
-
-// const cleanText = $('s').map(
-//     (i, el) => $(el).children().map(
-//         (i, el) => $(el).text()
-//     ).get().join('')
-// ).get();
-
-//console.log(cleanText.join(' '));
-
-//fs.writeFileSync('./cleanText.txt', cleanText.join(' '));
-
 const clean = textsLocations => iterateThroughFiles(textsLocations)(
     (text, textLocation) => {
         const $ = cheerio.load(text.toString(), {
